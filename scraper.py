3c88f8271dd3abe919d4bd6a6de6021867f90d90
adams <adamsliu97@gmail.com>
2020-12-20 17:52:21 -0500

    created virtual env

--- scraper.py
+++ scraper.py
@@ -1,33 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 import timeit
 
 
-options = webdriver.ChromeOptions()
-options.headless = True
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-driver.get("https://www.crudemonitor.ca/crudes/dist.php?acr=MSY&time=recent")
-celcius_btn = driver.find_elements_by_css_selector("label.btn-seconday")[0]
-celcius_btn.click()
+# load up a chrome driver object
+def load_chrome_driver():
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    return driver
 
 
-def test2():
-    # locate temperature element using css_selector (fastes)
-    temp = driver.find_elements_by_css_selector(
-        "#tblData div.table-responsive td.celsius")
-    tempLs = []
+# run selenium to get the most recent oil distillation temperataures (in C)
+def get_distillation_temps(driver, url):
 
-    for i in range(3, 39):
-        if (i+1) % 3 == 1:
-            tempLs.append(temp[i].text)
+    try:
+        driver.get(url)
+        celcius_btn = driver.find_elements_by_css_selector(
+            "label.btn-seconday")[0]
+        celcius_btn.click()
 
-    print(tempLs)
+        # scrape all celcius temperature values in the distallation table using selenium's css_selector
+        temps = driver.find_elements_by_css_selector(
+            "#tblData div.table-responsive td.celsius")
+
+        # parse through the scraped data and return a list of the all the temperatures for the first column (recent data)
+        temp_list = []
+        for i in range(3, 39):
+            if (i+1) % 3 == 1:
+                temp_list.append(temps[i].text)
+        return temp_list
+
+    except:
+        print("**Error: invalid url or invalid driver module**")
+        return []
 
 
-t = timeit.timeit("test2()", setup="from __main__ import test2", number=1)
-print(t)
+# t = timeit.timeit("test2()", setup="from __main__ import test2", number=1)
+# print(t)
+
+url = "https://www.crudemonitor.ca/crudes/dist.php?acr=MSY&time=recent"
+driver = load_chrome_driver()
+temp = get_distillation_temps(driver, url)
+print(temp)
